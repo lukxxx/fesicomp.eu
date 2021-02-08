f7015d62cfe4abb379704648dfbb9c8040d19c25
luxxx <jahodaluk82@gmail.com>
2021-02-08 15:36:54 +0100

    zmenené sql querries

--- template/search-engine.php
+++ template/search-engine.php
@@ -1,16 +1,15 @@
 <?php
-include "config.php";
+include "../config.php";
  
-if(isset($_REQUEST["term"])){
+if(isset($_REQUEST["term"]) && strlen($_REQUEST['term']) >= 3){
     $var = $_REQUEST["term"];
     $likeVar = "%" . $var . "%";
-    $sql = "SELECT * FROM produkty WHERE produkty LIKE ? LIMIT 5";
-    $kat = "SELECT * FROM produkty WHERE kategorie LIKE ? LIMIT 3";
+    $sql = "SELECT * FROM produkty WHERE p_nazov LIKE ? LIMIT 5";
+    $kat = "SELECT * FROM kategorie WHERE k_nazov LIKE ? LIMIT 3";
     
     if($stmt = mysqli_prepare($link, $sql)){
         mysqli_stmt_bind_param($stmt, "s", $likeVar);
         
-        // Set parameters
         $var = $_REQUEST["term"] . '%';
 
         if(mysqli_stmt_execute($stmt)){
@@ -21,9 +20,9 @@ if(isset($_REQUEST["term"])){
                   echo "<span style='font-weight: bold; font-size: 15px'><i class='fas fa-arrow-right'></i> Produkty</span>";
                   echo "<hr>";
                 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                  echo "<div style'display: flex'>";
-                     echo "<a href='' style='color: black'><p style='font-size: 15px'><img src='obrazok.jpg' width='30' height='20'><span style='padding-left: 10px'>" . $row["produkty"] . "</span>";
-                     echo "<span style='float: right; font-weight: bold; color: red;'>".$row['cena']."</span></p></a>";
+                  echo "<div style'display: flex; text-overflow: ellipsis; width: 80%'>";
+                     echo "<a href='' style='color: black'><p style='font-size: 15px'><img src='obrazok.jpg' width='30' height='20'><span style='padding-left: 10px'>" . $row["p_nazov"] . "</span>";
+                     echo "<span style='float: right; font-weight: bold; color: red;'>".$row['p_cena']."€</span></p></a>";
                    echo "</div>";
               }
             } else{
@@ -46,7 +45,7 @@ if(isset($_REQUEST["term"])){
                 echo "<span style='font-weight: bold; font-size: 15px'><i class='fas fa-arrow-right'></i> Kategórie</span>";
                 echo "<hr>";
               while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                   echo "<a href='' style='color: black'><p style='font-size: 15px'><i class='fas fa-folder-open'></i><span style='padding-left: 10px'>". $row["kategorie"] . "</span></a>";
+                   echo "<a href='' style='color: black'><p style='font-size: 15px'><i class='fas fa-folder-open'></i><span style='padding-left: 10px'>". $row["k_nazov"] . "</span></a>";
 
             }
           } else {
@@ -60,74 +59,9 @@ if(isset($_REQUEST["term"])){
    }
      
     mysqli_stmt_close($stmt);
+} else if(isset($_REQUEST["term"]) && strlen($_REQUEST['term']) <= 3){
+    echo "Píšte ďalej...";
 }
  
-mysqli_close($link);
-?>
-<?php
-include "config.php";
- 
-if(isset($_REQUEST["term"])){
-    $var = $_REQUEST["term"];
-    $likeVar = "%" . $var . "%";
-    $sql = "SELECT * FROM produkty WHERE produkty LIKE ? LIMIT 5";
-    $kat = "SELECT * FROM produkty WHERE kategorie LIKE ? LIMIT 3";
-    
-    if($stmt = mysqli_prepare($link, $sql)){
-        mysqli_stmt_bind_param($stmt, "s", $likeVar);
-        
-        // Set parameters
-        $var = $_REQUEST["term"] . '%';
-        
-        if(mysqli_stmt_execute($stmt)){
-            $result = mysqli_stmt_get_result($stmt);
-            
-            if(mysqli_num_rows($result) > 0){
-                  echo "<hr style='margin: 0; margin-bottom: 2%;  border: 0.5px solid black;}'>";
-                  echo "<span style='font-weight: bold; font-size: 15px'><i class='fas fa-arrow-right'></i> Produkty</span>";
-                  echo "<hr>";
-                while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                  echo "<div style'display: flex'>";
-                     echo "<a href='' style='color: black'><p style='font-size: 15px'><img src='obrazok.jpg' width='30' height='20'><span style='padding-left: 10px'>" . $row["produkty"] . "</span>";
-                     echo "<span style='float: right; font-weight: bold; color: red;'>".$row['cena']."</span></p></a>";
-                   echo "</div>";
-              }
-            } else{
-               echo "<span style='font-weight: bold; font-size: 15px'><i class='fas fa-arrow-right'></i> Produkty</span>";
-               echo "<hr>";
-               echo "<p>Nič sme nenašli</p>";
-            }
-        } else{
-            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-        }
-    }if($stmt = mysqli_prepare($link, $kat)){
-      mysqli_stmt_bind_param($stmt, "s", $param_term);
-      
-      // Set parameters
-      $param_term = $_REQUEST["term"] . '%';
-      
-      if(mysqli_stmt_execute($stmt)){
-          $result = mysqli_stmt_get_result($stmt);
-          
-          if(mysqli_num_rows($result) > 0){
-                echo "<span style='font-weight: bold; font-size: 15px'><i class='fas fa-arrow-right'></i> Kategórie</span>";
-                echo "<hr>";
-              while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                   echo "<a href='' style='color: black'><p style='font-size: 15px'><i class='fas fa-folder-open'></i><span style='padding-left: 10px'>". $row["kategorie"] . "</span></a>";
-
-            }
-          } else {
-            echo "<span style='font-weight: bold; font-size: 15px'><i class='fas fa-arrow-right'></i> Kateórie</span>";
-            echo "<hr>";
-            echo "<p>Nič sme nenašli</p>";
-          }
-      } else{
-          echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-      }
-   }
-
-    mysqli_stmt_close($stmt);
-}
-
 mysqli_close($link);
 ?>
