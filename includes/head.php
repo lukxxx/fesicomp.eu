04b8e128ec882b385740e889c2ce71485500cdd4
luxxx <jahodaluk82@gmail.com>
2021-02-08 15:36:21 +0100

    cesty zmenené

--- includes/head.php
+++ includes/head.php
@@ -1,10 +1,11 @@
 <head>
 <meta charset="UTF-8">
 <title>fesicomp.eu</title>
-<link rel="stylesheet" href="bootstrap.css">
+<link rel="stylesheet" href="assets/css/bootstrap.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="assets/css/style.css">
 <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">  
 <link href='https://fonts.googleapis.com/css?family=Product+Sans' rel='stylesheet' type='text/css'>
-<script src="assets/search.js" type="text/javascript"></script>
+<script src="assets/js/search.js" type="text/javascript"></script>
 </head>
