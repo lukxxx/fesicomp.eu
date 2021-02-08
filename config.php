d12b312ca7c1f421e1b7dbb4c00fbc5835490b62
luxxx <jahodaluk82@gmail.com>
2021-02-08 15:35:26 +0100

    databáza miškova

--- config.php
+++ config.php
@@ -1,21 +1,11 @@
 <?php 
-$hostname = "localhost";
+$hostname = "87.197.174.134";
 $user = "root";
 $pass = "";
-$db_name = "search";
+$db_name = "compsnv_sk2";
 $link = mysqli_connect($hostname, $user, $pass, $db_name);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
 
-<?php /*
-$hostname = "localhost";
-$user = "root";
-$pass = "";
-$db_name = "search";
-$link = mysqli_connect($hostname, $user, $pass, $db_name);
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-?>
