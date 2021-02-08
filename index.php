fc659bb19efc6375a7dd67552e45220aa37767a4
luxxx <jahodaluk82@gmail.com>
2021-02-08 16:51:39 +0100

    pridané kategorie naboku

--- index.php
+++ index.php
@@ -4,6 +4,13 @@
 <body>
     <?php include "includes/header.php" ?>
     <?php include "includes/index-carousel.php"; ?>
+    <div class="container" style="padding-top: 20px">
+        <div class="row">
+            <div class="col-sm-12 col-md-3 col-lg-3">
+                <?php include "includes/category-list.php" ?>
+            </div>
+        </div>
+    </div>
     <?php include "includes/footer.php"?>
     <?php include "includes/scripts.php"?>
 </body>
