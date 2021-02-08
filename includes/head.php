a59c72efc0bb163a88f8271e6925f775d815cbfa
luxxx <jahodaluk82@gmail.com>
2021-02-08 14:17:47 +0100

    pridaná hlavička na includovanie

---
+++ includes/head.php
@@ -0,0 +1,10 @@
+<head>
+<meta charset="UTF-8">
+<title>fesicomp.eu</title>
+<link rel="stylesheet" href="bootstrap.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+<link rel="stylesheet" href="style.css">
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<link href='https://fonts.googleapis.com/css?family=Product+Sans' rel='stylesheet' type='text/css'>
+<script src="assets/search.js" type="text/javascript"></script>
+</head>
