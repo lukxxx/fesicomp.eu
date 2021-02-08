127ae7b55008a91eebd64fac21dd1604a9d94828
luxxx <jahodaluk82@gmail.com>
2021-02-08 14:06:46 +0100

    pridanie modulov na index

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="sk">
+<?php include "includes/head.php";?>
+<body>
+    <?php include "includes/header.php" ?>
+    <?php include "includes/index-carousel"; ?>
+    <?php include "includes/footer.php"?>
+    <?php include "includes/scripts.php"?>
+</body>
+</html>
