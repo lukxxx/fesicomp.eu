cc2727c07f4d45c2dc5fee87bcc67685fe62fa3e
luxxx <jahodaluk82@gmail.com>
2021-02-09 15:57:31 +0100

    opravvená classa

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,7 @@
                             <div class="award">
                                 <a href="index.php" style="padding-left: 20px;height: 90px;"><img src="assets/images/brand/skusenosti.gif" width="95" height="90"></a>
                             </div>
-                            <div class="headings">
+                            <div class="header-headings">
                                 <h2>VÝPOČTOVÁ TECHNIKA</h2>
                                 <h4>predaj a servis</h4>
                                 <div class="quick-links">
