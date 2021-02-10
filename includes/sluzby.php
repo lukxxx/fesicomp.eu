3ecf3208704949ac5915f0fb0425b870b1421d11
luxxx <jahodaluk82@gmail.com>
2021-02-10 15:01:19 +0100

    priadaná sekcia na služby

---
+++ includes/sluzby.php
@@ -0,0 +1,48 @@
+<div class="col-sm-12 col-md-3 col-lg-3">
+                    <div class="product-card justify-content-md-center">
+                        <div class="product-img justify-content-md-center">
+                            <img src="assets/images/sluzby/zostavy.jpg" width="159" class="img-prod" height="120">
+                        </div>
+                        <div class="product-nam justify-content-md-center">
+                            <div class="headin">
+                                <h6 class="name-prod">Konfigurátor zostáv</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-3 col-lg-3">
+                    <div class="product-card justify-content-md-center">
+                        <div class="product-img justify-content-md-center">
+                            <img src="assets/images/sluzby/opravy.jpg" width="159" class="img-prod" height="120">
+                        </div>
+                        <div class="product-nam justify-content-md-center">
+                            <div class="headin">
+                                <h6 class="name-prod">Oprava počítačov</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-3 col-lg-3">
+                    <div class="product-card justify-content-md-center">
+                        <div class="product-img justify-content-md-center">
+                            <img src="assets/images/sluzby/skylink.jpg" width="130" class="img-prod" height="120">
+                        </div>
+                        <div class="product-nam justify-content-md-center" style=" padding: 20px;">
+                            <div class="headin">
+                                <h6 class="name-prod">Aktivujeme platené služby Skylink</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-3 col-lg-3">
+                    <div class="product-card justify-content-md-center">
+                        <div class="product-img justify-content-md-center">
+                            <img src="assets/images/sluzby/ekasy.jpg" width="120" class="img-prod" height="90">
+                        </div>
+                        <div class="product-nam justify-content-md-center" style="text-align: center; padding: 50px;">
+                            <div class="heading">
+                                <h6 class="name-prod">eKasy</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
