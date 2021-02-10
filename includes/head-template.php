c4a04e0af0cfe58a247df76bb295258e4c59387f
luxxx <jahodaluk82@gmail.com>
2021-02-10 14:58:29 +0100

    vytvorené sekundárne súbory pre template

---
+++ includes/head-template.php
@@ -0,0 +1,12 @@
+<?php include "dir.php"?>
+<head>
+<meta charset="UTF-8">
+<title>screenshieldy.eu</title>
+<link rel="stylesheet" href="../assets/css/bootstrap.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+<link rel="stylesheet" href="../assets/css/style.css">
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">  
+<link href='https://fonts.googleapis.com/css?family=Product+Sans' rel='stylesheet' type='text/css'>
+<script src="../assets/js/search-template.js" type="text/javascript"></script>
+</head>
