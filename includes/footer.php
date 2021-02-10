962ed5bd44f7ffcc4dc8508b0e799a96c3269514
luxxx <jahodaluk82@gmail.com>
2021-02-10 15:00:21 +0100

    pridaný footer

---
+++ includes/footer.php
@@ -0,0 +1,41 @@
+<div class="container-fluid" style="background-color: #2B2B2B;">
+        <div class="container">
+            <footer id ="footer">
+                <div class = "row">
+                    <div class="col-sm-12 col-md-3 col-lg-3" style="padding-top: 2vw">
+                        <span class="footer-h">O nás</span><br>
+                        <a>Fotogaléria</a><br>
+                        <a>Certifikáty</a><br>
+                        <a>Obchodné podmienky</a><br>
+                        <a>Kontakt</a><br>
+                    </div>
+                    <div class="col-sm-12 col-md-3 col-lg-3" style="padding-top: 2vw">
+                        <span class="footer-h">Sledujte nás</span><br>
+                        <a>Facebook <i class="fab fa-facebook"></i></a><br>
+                        <span class="footer-h">U nás zaplatíte:</span><br>
+                        <i class="fab fa-cc-visa"></i>
+                        <i class="fab fa-cc-mastercard"></i>
+                        <i class="fas fa-exchange-alt fa-1x"></i>&nbsp;<span style="font-size:14px;">Bankovým prevodom</span>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-6" style="display: flex; padding-top: 2vw">
+                        <div class="location" style="display: block; padding-right: 3vw">
+                            <span class="footer-h">Kde nás nájdete?</span><br>
+                            <span>FESI comp, s.r.o.<br>
+                                    Duklianska 3A<br>
+                                    052 01 Spišská Nová Ves<br>
+                                    Tel.: 053 / 44 11 526<br>
+                                    mail: fesicomp@fesicomp.sk</span><br>
+                        </div>    
+                        <div class="map">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2620.335510327889!2d20.55549281590515!3d48.947097002410366!2m3!1f0!2f0!
+                                3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473e48d579e67313%3A0x88322d6a5d537bd3!2sFESI%20comp%2C%20s.r.o.!5e0!3m2!1ssk!2ssk!4v1612459364910!5m2!1ssk!2ssk" 
+                                width="300" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+                <span style="text-align: center">Produced by KaJa UhRoBa</span> 
+            </footer>
+        </div>
+        
+    </div>
