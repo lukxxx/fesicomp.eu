a6360fc2ec3325dda37e97d34ffb0bdd77dbdbd7
uhrinalex <uhrin.alex60@gmail.com>
2021-02-17 00:38:30 +0100

    link pre ikonu v tlacidle odoslat

--- includes/head-template.php
+++ includes/head-template.php
@@ -11,7 +11,12 @@
     <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
     <link href='https://fonts.googleapis.com/css?family=Product+Sans' rel='stylesheet' type='text/css'>
     <script src="../assets/js/search-template.js" type="text/javascript"></script>
-    
+
+    <!-- Tento link pridal aleks pre ikonku v contact.php -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Tento -->
+
+
     <meta name="google-signin-client_id" content="238466960669-o9vmi5uorbemeudllt4f5chf5auq0kia.apps.googleusercontent.com">
     <script src="https://apis.google.com/js/platform.js" ></script>
     <script>
