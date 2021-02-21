486bc231ea7a4a2bbab42f249959280172da71d0
luxxx <jahodaluk82@gmail.com>
2021-02-21 19:39:16 +0100

    zmenený meta title

--- includes/head-template.php
+++ includes/head-template.php
@@ -3,7 +3,7 @@
 <html lang="sk">
 <head>
     <meta charset="UTF-8">
-    <title>screenshieldy.eu</title>
+    <title>fesicomp.eu</title>
     <link rel="stylesheet" href="../assets/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
     <link rel="stylesheet" href="../assets/css/style.css">
@@ -22,4 +22,6 @@
     
 </head>
 <body>
+<?php
 
+?>
