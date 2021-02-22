637d396d44fd84b9e6d8bb040f6f92c207adfc23
luxxx <jahodaluk82@gmail.com>
2021-02-22 18:17:39 +0100

    odstránené nadbytočné linky

--- includes/index-carousel.php
+++ includes/index-carousel.php
@@ -1,17 +1,18 @@
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<?php
+require_once "head.php";
+?>
+
 <section class="carousel" >
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                <img class="d-block  mx-auto" src="assets/images/carousel/techsavers1.jpg" alt="First slide">
+                <img class="d-block  mx-auto" src="assets/images/carousel/ekasa1.jpg" alt="First slide">
                 </div>
                 <div class="carousel-item">
                 <img class="d-block  mx-auto" src="assets/images/carousel/techsavers2.jpg" alt="Second slide">
                 </div>
                 <div class="carousel-item">
-                <img class="d-block  mx-auto" src="assets/images/carousel/ekasa1.jpg" alt="Third slide">
+                <img class="d-block  mx-auto" src="assets/images/carousel/techsavers1.jpg" alt="Third slide">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
