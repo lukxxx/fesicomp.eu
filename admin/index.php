60e08fa55baebaf65750e8fe59bfa1afaeebfcb8
Michalbarilla <michalbarila4@gmail.com>
2021-02-23 23:54:36 +0100

    admin login

---
+++ admin/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="sk">
+    <?php include "includes/head.php"?>
+<body class="login-bg ">
+    <div class="justify-content-center d-flex align-items-center align-items-center">
+        <div class="container-login ">
+            <img src="assets/images/logo.png" alt="logo" width="100px" style="margin: 0px 15px 15px 0px; float: left;">
+            <h2 style="color:white">Admin</h2>
+            <h2 style="color:white">Login</h2>
+            <form class="" method="post">
+                <div class="form-group">
+                    <input class="form-control input-lg" type="login" name="login" id="login" placeholder="login" />
+                </div>
+                <div class="form-group">
+                    <input class="form-control input-lg" type="heslo" name="heslo" placeholder="heslo" />
+                </div>
+                <div class="form-group text-center ">
+                    <input type="submit" name="submit" class="btn btn-light btn-lg" value="Sign Up" />
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
