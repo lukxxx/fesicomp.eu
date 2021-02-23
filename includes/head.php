107ba6168dec73ad9fc01a74e61777e8415c4d92
uhrinalex <uhrin.alex60@gmail.com>
2021-02-24 00:20:50 +0100

    pridaný meta content device width

--- includes/head.php
+++ includes/head.php
@@ -7,6 +7,7 @@
 <html lang="sk">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>FESI comp, s.r.o</title>
 <link rel="stylesheet" href="assets/css/bootstrap.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
