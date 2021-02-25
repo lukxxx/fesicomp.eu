2c04f12a92af3ee3dcc6ce5e4c80f81ae3bdb19b
uhrinalex <uhrin.alex60@gmail.com>
2021-02-25 11:44:19 +0100

    vymazane zbytocne linky

--- includes/head.php
+++ includes/head.php
@@ -6,23 +6,30 @@
 <!DOCTYPE html>
 <html lang="sk">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>FESI comp, s.r.o</title>
-<link rel="stylesheet" href="assets/css/bootstrap.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-<link rel="stylesheet" href="assets/css/style.css">
-<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">  
-<link href='https://fonts.googleapis.com/css?family=Product+Sans' rel='stylesheet' type='text/css'>
-<script src="assets/js/search.js" type="text/javascript"></script>
-<script src="assets/js/bootstrap.js" type="text/javascript"></script>
-<meta name="google-signin-client_id" content="238466960669-o9vmi5uorbemeudllt4f5chf5auq0kia.apps.googleusercontent.com">
-<script src="https://apis.google.com/js/platform.js"></script>
-<script>
-        gapi.load('auth2', function(){
-            gapi.auth2.init();
-        });
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FESI comp, s.r.o</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">  
+    <link href='https://fonts.googleapis.com/css?family=Product+Sans' rel='stylesheet' type='text/css'>
+    <script src="assets/js/search.js" type="text/javascript"></script>
+
+
+    
+
+
+
+
+    <script src="assets/js/bootstrap.js" type="text/javascript"></script>
+    <meta name="google-signin-client_id" content="238466960669-o9vmi5uorbemeudllt4f5chf5auq0kia.apps.googleusercontent.com">
+    <script src="https://apis.google.com/js/platform.js"></script>
+    <script>
+            gapi.load('auth2', function(){
+                gapi.auth2.init();
+            });
     </script>
 </head>
 <body>
