23bdd6b5928ffd81851e1aa1b0360ac541e5131d
uhrinalex <uhrin.alex60@gmail.com>
2021-02-25 11:32:40 +0100

    mapa vo footri na samostatny riadok

--- includes/footer.php
+++ includes/footer.php
@@ -17,7 +17,7 @@
                         <i class="fab fa-cc-mastercard"></i><span> Mastercard</span><br>
                         <i class="fas fa-exchange-alt fa-1x"></i>&nbsp;<span style="font-size:16px;">Bankovým prevodom</span>
                     </div>
-                    <div class="col-sm-12 col-md-6 col-lg-6" style="display: flex; padding-top: 1vw">
+                    <div class="col-sm-12 col-md-3 col-lg-3" style="display: flex; padding-top: 1vw">
                         <div class="location" style="display: block; padding-right: 3vw">
                             <span class="footer-h">Adresa</span><br>
                             <span>FESI comp, s.r.o.<br>
@@ -26,13 +26,14 @@
                                     053 / 44 11 526<br>
                                     fesicomp@fesicomp.sk</span><br>
                         </div>    
+                    </div>
+                    <div class="col-sm-12 col-md-3 col-lg-3" style="padding-top: 1vw">
                         <div class="map">
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2620.335510327889!2d20.55549281590515!3d48.947097002410366!2m3!1f0!2f0!
                                 3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473e48d579e67313%3A0x88322d6a5d537bd3!2sFESI%20comp%2C%20s.r.o.!5e0!3m2!1ssk!2ssk!4v1612459364910!5m2!1ssk!2ssk" 
                                 width="100%" height="150" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
                             </iframe>
                         </div>
-
                     </div>
                 </div>
                <hr style="background-color:white;">
