a016129160ac1a9cc0d2baf4791ec9be1df57fce
Matej Kalavsky <matej.kalavsky28@gmail.com>
2021-02-26 23:39:54 +0100

    sluzby

--- includes/sluzby.php
+++ includes/sluzby.php
@@ -1,27 +1,68 @@
-<div class="col-sm-12 col-md-3 col-lg-3">
-                    <div class="product-card justify-content-md-center">
+
+
+
+<div class = "col-sm-12 col-md-6 col-lg-6">
+            <div class="alu-card justify-content-md-center">
                         <div class="product-img justify-content-md-center">
-                            <img src="assets/images/sluzby/zostavy.jpg" width="159" class="img-prod" height="120">
+                            <img src="assets/images/sluzby/skylink.jpg" width="130" class="img-prod">
                         </div>
-                        <div class="product-nam justify-content-md-center">
+                        <div class="product-nam justify-content-md-center" style=" padding: 20px;">
                             <div class="headin">
-                                <h6 class="name-prod">Konfigurátor zostáv</h6>
+                                <h6 class="name-prod">Aktivujeme platené služby Skylink</h6>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-sm-12 col-md-3 col-lg-3">
-                    <div class="product-card justify-content-md-center">
+
+
+
+</div>
+<div class = "col-sm-12 col-md-6 col-lg-6 parent" >
+    <div class = "row child">
+        <div class = "col-sm-12 col-md-12 col-lg-12" ><div class="alu-card justify-content-md-center">
                         <div class="product-img justify-content-md-center">
-                            <img src="assets/images/sluzby/opravy.jpg" width="159" class="img-prod" height="120">
+                            <img src="assets/images/sluzby/skylink.jpg" width="130" class="img-prod" >
                         </div>
-                        <div class="product-nam justify-content-md-center">
+                        <div class="product-nam justify-content-md-center" style=" padding: 20px;">
                             <div class="headin">
-                                <h6 class="name-prod">Oprava počítačov</h6>
+                                <h6 class="name-prod">Aktivujeme platené služby Skylink</h6>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    </div></div>
+            
+            
+    
+    
+    </div>
+
+    <div class = "row child">
+        <div class = "col-sm-12 col-md-6 col-lg-6"><div class="alu-card justify-content-md-center">
+                        <div class="product-img justify-content-md-center">
+                            <img src="assets/images/sluzby/skylink.jpg" width="130" class="img-prod" >
+                        </div>
+                        <div class="product-nam justify-content-md-center" style=" padding: 20px;">
+                            <div class="headin">
+                                <h6 class="name-prod">Aktivujeme platené služby Skylink</h6>
+                            </div>
+                        </div>
+                    </div></div>
+        <div class = "col-sm-12 col-md-6 col-lg-6"><div class="alu-card justify-content-md-center">
+                        <div class="product-img justify-content-md-center">
+                            <img src="assets/images/sluzby/skylink.jpg" width="130" class="img-prod" >
+                        </div>
+                        <div class="product-nam justify-content-md-center" style=" padding: 20px;">
+                            <div class="headin">
+                                <h6 class="name-prod">Aktivujeme platené služby Skylink</h6>
+                            </div>
+                        </div>
+                    </div></div>
+    </div>
+
+
+</div>
+
+
+<!--
+                
                 <div class="col-sm-12 col-md-3 col-lg-3">
                     <div class="product-card justify-content-md-center">
                         <div class="product-img justify-content-md-center">
@@ -46,3 +87,5 @@
                         </div>
                     </div>
                 </div>
+
+-->
