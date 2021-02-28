a5d92ff27d1695b8170d3eaf5fd454898a95b2fc
uhrinalex <uhrin.alex60@gmail.com>
2021-03-01 00:45:16 +0100

    mobilny cart <3

--- template/cart.php
+++ template/cart.php
@@ -2,7 +2,7 @@
 include "../includes/header-template.php";
 ?>
 
-<div class="container" style="margin-top: 50px;">
+<div class="container cart_desktop" style="margin-top: 50px;">
     <div class="row d-flex">
         <div class="col-sm-12 col-md-3 col-lg-3">
             <a style="color: black;" href="<?php echo $_SERVER['HTTP_REFERER'] ?>"><i class="fas fa-arrow-left"></i> Pokračovať v nákupe</a>
@@ -108,6 +108,114 @@ include "../includes/header-template.php";
         </div>
     </div>
 </div>
+
+
+<!-- mobilny kosik -->
+<div class="container cart_mobile" style="margin-top: 50px;">
+    <div class="row d-flex">
+        <div class="col-sm-12 col-md-3 col-lg-3" style="margin: 0 25px;">
+            <a style="color: black;" href="<?php echo $_SERVER['HTTP_REFERER'] ?>"><i class="fas fa-arrow-circle-left"></i> Pokračovať v nákupe</a>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6 text-center" style="margin: 10px 0;">
+            <h2 style="font-weight: bold;">KOŠÍK</h2>
+        </div>
+        <div class="col-sm-12 col-md-3 col-lg-3" style="text-align: right; margin: 0 25px;">
+            <?php if(isset($_COOKIE['cart']) && $_COOKIE['cart'] != "[]"){ ?>
+            <a style="color: black; text-align: right;" href="data.php">Pokračovať k objednávke <i class="fas fa-arrow-circle-right"></i></a>
+            <?php
+            }
+            ?>
+            <hr>
+            <br>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12 text-center">
+
+    <?php
+    if(isset($_COOKIE['cart']) && $_COOKIE['cart'] != "[]"){
+        $cart = isset($_COOKIE["cart"]) ? $_COOKIE["cart"] : "[]";
+        $cart = json_decode($cart);
+        $total = 0;
+        foreach ($cart as $c)
+        {
+            $total += $c->product->p_cena * $c->quantity;
+            ?>  
+            <div style="margin: 0 25px;">
+                <div class="d-flex justify-content-between">
+                    <?php echo "<a style='color: black; font-size: 15px;' class='text-left' href='item.php?ID=".$c->product->p_id."'>".$c->product->p_nazov."</a>" ?>
+                    
+                    <form method="POST" action="../delete-cart.php">
+                        <input type="hidden" name="productCode" value="<?php echo $c->productCode; ?>">
+                        <button type="submit" name="delete" style="all: unset; cursor: pointer;"><i style="color: #C21800; font-size: 30px; margin-right: 15px;" class="fas fa-times fa-1x"></i></button>
+                    </form>
+                </div>
+
+                <p class="text-left"><?php if($c->product->p_sklad == 1){ echo "<span style='color: #149106'>Skladom</span>"; } ?></p>
+                
+                <div class="d-flex justify-content-between align-items-center">
+                    <form method="post" action="../update-cart.php">
+                        <button type="submit" name="quantity-minus" style="all: unset; cursor: pointer;"><i class="fas fa-minus"></i></button>
+                        <input style="all: unset; width: 20%;" type="number" id="quantity" name="quantity" min="1" value="<?php echo $c->quantity; ?>">
+                        <input type="hidden" name="productCode" value="<?php echo $c->productCode; ?>">
+                        <button type="submit" name="quantity-plus" style="all: unset; cursor: pointer;"><i class="fas fa-plus"></i></button>
+                    </form>
+
+                    <span style="padding-right: 25px; font-size: 21px;"><strong style="color: #C21800;"><?php echo $c->product->p_cena * $c->quantity; ?>€</strong></span>
+                </div>
+
+
+                <hr>
+
+            </div>
+            <?php
+        }
+    } else {
+        $total = 0;
+        echo "<span style='text-align: center; font-weight: bold;'>Nákupný košík je prázdny!</span>";
+    }
+    
+    ?>
+
+        </div>
+    </div>
+    <div class="row" style="margin: 20px 10px;">
+        <div class="col-sm-12 col-sm-8 col-lg-8">
+            <?php 
+             if(isset($total) && $total != 0){ 
+                $no_dph = ($total / 100) * 80; 
+                $nodph = number_format($no_dph, 2, ',', ' ');
+                ?>
+            <span style="font-weight: bold; font-size: 20px;">Uplatniť zľavový kód</span>
+            <div class="d-flex input-code" style="padding-top: 10px;">
+                <form method="POST" action="#" style="display: flex;">
+                    <input type="text" placeholder="FESI" name="code" class="form-control"><button style="background-color: #B81600; color: white; border-radius: 10px;
+                     padding: 0px 20px 0px 20px; margin-left: 10px;" type="submit" class="btn">Uplatniť</button>
+                </form>
+            </div>
+            <?php 
+            }?>
+        </div>
+        <div class="col-sm-12 col-sm-4 col-lg-4" style="text-align: right; ">
+            <?php 
+            if(isset($total) && $total != 0){ 
+            $no_dph = ($total / 100) * 80; 
+            $nodph = number_format($no_dph, 2, ',', ' ');
+            ?>
+            <span style="margin-top: 10px;"><strong style="font-size: 18px; padding-right: 40px">Cena spolu: </strong><span style="font-size: 25px; font-weight: bold; color: #C5230D;"><?php echo $total."€";  ?></span></span><br>   
+            <span style="color: #636363; padding-right: 90px; padding-top: 20px;">Cena bez DPH: </span><span style="color: #636363;"><?php echo $nodph."€"; ?></span>
+            <?php 
+            }?>
+        </div>
+    </div>
+</div>
+
+<!-- END mobilny kosik -->
+
+
+
+
 <div class="container">
     <div class="row" style="padding-top: 50px;">
     <?php 
