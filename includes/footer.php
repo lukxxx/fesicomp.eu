5fc369c0fcf29a067e8243093da31956abf0f067
luxxx <jahodaluk82@gmail.com>
2021-03-03 19:45:23 +0100

    zmenená mapa na obrázok

--- includes/footer.php
+++ includes/footer.php
@@ -11,16 +11,16 @@
         <div class="container">
             <footer id ="footer">
                 <div class = "row">
-                    <div class="col-sm-12 col-md-3 col-lg-3" style="padding-top: 1vw">
+                    <div class="col-sm-12 col-md-3 col-lg-3" id="footer_onas" style="padding-top: 1vw">
                         <span class="footer-h">O nás</span><br>
                         <a href="<?php echo $odkaz?>about.php" style="color: white; text-decoration: none;">O spoločnosti</a><br>
                         <a href="contact.php" style="color: white; text-decoration: none;">Kontakt</a><br>
                         <a href="<?php echo $odkaz?>subpage.php?ID=3" style="color: white; text-decoration: none;">Obchodné podmienky</a><br>
                         <a href="<?php echo $odkaz?>subpage.php?ID=4" style="color: white; text-decoration: none;">Reklamácia a vrátenie</a><br>
                         <a href="<?php echo $odkaz?>subpage.php?ID=5" style="color: white; text-decoration: none;">Ochrana osobných údajov</a><br>
-                        
+
                     </div>
-                    <div class="col-sm-12 col-md-3 col-lg-3" style="padding-top: 1vw">
+                    <div class="col-sm-12 col-md-3 col-lg-3" id="footer_follow" style="padding-top: 1vw">
                         <span class="footer-h">Sledujte nás</span><br>
                         <a href = "https://www.facebook.com/fesicomp.sk" style = "color:white;">Facebook <i class="fab fa-facebook"></i></a><br>
                         <span class="footer-h">U nás zaplatíte</span><br>
@@ -28,7 +28,7 @@
                         <i class="fab fa-cc-mastercard"></i><span> Mastercard</span><br>
                         <i class="fas fa-exchange-alt fa-1x"></i>&nbsp;<span style="font-size:16px;">Bankovým prevodom</span>
                     </div>
-                    <div class="col-sm-12 col-md-3 col-lg-3" style="display: flex; padding-top: 1vw">
+                    <div class="col-sm-12 col-md-3 col-lg-3" id="footer_address" style="display: flex; padding-top: 1vw;">
                         <div class="location" style="display: block; padding-right: 3vw">
                             <span class="footer-h">Adresa</span><br>
                             <span>FESI comp, s.r.o.<br>
@@ -38,13 +38,12 @@
                                     fesicomp@fesicomp.sk</span><br>
                         </div>    
                     </div>
-                    <div class="col-sm-12 col-md-3 col-lg-3" style="padding-top: 1vw">
-                        <div class="map">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2620.335510327889!2d20.55549281590515!3d48.947097002410366!2m3!1f0!2f0!
-                                3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473e48d579e67313%3A0x88322d6a5d537bd3!2sFESI%20comp%2C%20s.r.o.!5e0!3m2!1ssk!2ssk!4v1612459364910!5m2!1ssk!2ssk" 
-                                width="100%" height="150" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
-                            </iframe>
-                        </div>
+                    <div class="col-sm-12 col-md-4 col-lg-4" style="padding-top: 1vw">
+                        <a target="_blank" href="https://goo.gl/maps/69n38NnoHfmMWA8E8" style="">
+                            
+                                <img src="..\assets\images\mapa.png" target="_blank" alt="mapa" style="width: 70%; margin-left: auto; margin-right: auto; display: block;">
+                            
+                        </a>
                     </div>
                 </div>
                <hr style="background-color:white;">
