86a85c7f658d28f7edee91f04f0a0988a0073276
luxxx <jahodaluk82@gmail.com>
2021-04-13 20:55:25 +0200

    Upravené linky na databázy

--- config.php
+++ config.php
@@ -8,21 +8,10 @@ $details = json_decode($details);
 
 define("ROOT", __DIR__ ."/");
 
-$db_host = "mariadb103.websupport.sk";
-$db_name = "compsnv_sk2";
-$db_user = "compsnv_sk2";
-$db_pass = "iQ8sh2lz";
-
-
 // Create a connection to the MySQL database using PDO
-$pdo = $pdo = new PDO("mysql:host=mariadb103.websupport.sk;port=3313;dbname=compsnv_sk2", "compsnv", "Kajauhroba#2021");
+$pdo = $pdo = new PDO("mysql:host=db.dw003.nameserver.sk;port=3306;dbname=compsnv_sk2", "compsnv_sk2", "iQ8sh2lz");
 
-
-$hostname = "mariadb103.websupport.sk";
-$user = "compsnv_sk2";
-$pass = "iQ8sh2lz";
-$db_name = "compsnv_sk2";
-$link = mysqli_connect("mariadb103.websupport.sk", "compsnv", "Kajauhroba#2021", "compsnv_sk2", 3313);
+$link = mysqli_connect("db.dw003.nameserver.sk", "compsnv_sk2", "iQ8sh2lz", "compsnv_sk2", 3306);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
