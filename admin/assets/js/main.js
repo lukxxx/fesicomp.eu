d57fcec3008d05684dede9d7ef1c011420fedc0a
luxxx <jahodaluk82@gmail.com>
2021-05-28 13:46:25 +0200

    pridané skripty na úpravu

--- admin/assets/js/main.js
+++ admin/assets/js/main.js
@@ -62,6 +62,16 @@ $(document).ready(function () {
             $('.edit_dost').hide();
         }
     )
+
+    $(".product-aktual").hover(
+        function () {
+            $('.edit_aktual').show();
+        },
+
+        function () {
+            $('.edit_aktual').hide();
+        }
+    )
     $(".product-cena").hover(
         function () {
             $('.edit_cena').show();
@@ -81,6 +91,36 @@ $(document).ready(function () {
             $('.edit_odpo').hide();
         }
     )
+    
+    $(".product-rabat").hover(
+        function () {
+            $('.edit_rabat').show();
+        },
+
+        function () {
+            $('.edit_rabat').hide();
+        }
+    )
+
+    $(".product-poplatok").hover(
+        function () {
+            $('.edit_poplatok').show();
+        },
+
+        function () {
+            $('.edit_poplatok').hide();
+        }
+    )
+
+    $(".product-popis").hover(
+        function () {
+            $('.edit_popis').show();
+        },
+
+        function () {
+            $('.edit_popis').hide();
+        }
+    )
 
     $(".edit-close").hover(
         function () {
@@ -95,6 +135,16 @@ $(document).ready(function () {
     $(".edit-save").hover(
         function () {
             $('.save_text').text('Uložiť');
+        },
+
+        function () {
+            $('.save_text').text('');
+        }
+    )
+
+    $('.akcia_sc').children(".edit-save").hover(
+        function () {
+            $('.save_text').text('Pridať');
         },
 
         function () {
@@ -257,6 +307,48 @@ $(document).ready(function () {
           }, 2000);
     })
 
+    $(".edit_aktual").click(function () {
+        $('.left').hide();
+        $('.right').hide();
+        let product_aktual_w = $('.product_aktual_span').width();
+        product_aktual_w = product_aktual_w + 20;
+        $('#product_aktual').show();
+        $('.product_aktual_sc').css('display', 'flex');
+        $('#product_aktual').width(product_aktual_w);
+        $('.product_aktual_span').hide();
+        $('.edit_aktual').addClass('edit_h_hide-small');
+        $('.edit_aktual').removeClass('edit-small');
+    })
+
+    $(".p_aktual_close").click(function () {
+        $('.left').show();
+        $('.right').show();
+        $('.edit_h_hide-small').addClass('edit-small');
+        $('.edit-small').removeClass('edit_h_hide-small');
+        $('#product_aktual').hide();
+        $('.product_aktual_sc').hide();
+        $('.product_aktual_span').show();
+        $('#product_aktual').val(product_aktual);
+    })
+    $(".p_aktual_save").click(function () {
+        let product_aktual_new = $('#product_aktual').val();
+        $('.edit_h_hide-small').addClass('edit-small');
+        $('.edit-small').removeClass('edit_h_hide-small');
+        $('#product_aktual').hide();
+        $('.product_aktual_sc').hide();
+        $('.product_aktual_span').text(product_aktual_new);
+        $('#product_aktual').attr('value', product_aktual_new);
+        $('.product_aktual_span').show();
+        $('.left').show();
+        $('.right').show();
+        $('.product-aktual').children(".saved-msg").animate({opacity: '0.8'});
+        $('.product-aktual').children(".saved-msg").css('height', '20px');
+        setTimeout(function(){
+            $('.product-aktual').children(".saved-msg").animate({opacity: '0'});
+            $('.product-aktual').children(".saved-msg").animate({height: '0px'});
+          }, 2000);
+    })
+
     $(".edit_cena").click(function () {
         $('.currency_cena').hide();
         let product_cena_w = $('.product_cena_span').width();
@@ -301,7 +393,7 @@ $(document).ready(function () {
     $(".edit_odpo").click(function () {
         $('.currency_odpo').hide();
         let product_odpo_w = $('.product_odpo_span').width();
-        $('#product_odpo').width(product_odpo_w);
+        $('#product_odpo').css('width','40%');
         $('#product_odpo').show();
         $('.product_odpo_sc').css('display', 'flex'); 
         $('.product_odpo_span').hide();
@@ -337,6 +429,150 @@ $(document).ready(function () {
             $('.product-odpo').children(".saved-msg").animate({opacity: '0'});
             $('.product-odpo').children(".saved-msg").animate({height: '0px'});
           }, 2000);
+    })
+
+    $(".edit_rabat").click(function () {
+        $('.currency_rabat').hide();
+        let product_rabat_w = $('.product_rabat_span').width();
+        $('#product_rabat').css('width','40%');
+        $('#product_rabat').show();
+        $('.product_rabat_sc').css('display', 'flex'); 
+        $('.product_rabat_span').hide();
+        let product_rabat = $('.product_rabat_span').text();
+        $('#product_rabat').val(product_rabat);
+        $('.edit-small').addClass('edit_h_hide-small');
+        $('.edit_h_hide-small').removeClass('edit-small');
+        
+    })
+
+    $(".p_rabat_close").click(function () {
+        $('.edit_h_hide-small').addClass('edit-small');
+        $('.edit-small').removeClass('edit_h_hide-small');
+        $('#product_rabat').hide();
+        $('.product_rabat_sc').hide();
+        $('.currency_rabat').show();
+        $('.product_rabat_span').show();
+        $('#product_rabat').val(product_rabat);
+    })
+    $(".p_rabat_save").click(function () {
+        let product_rabat_new = $('#product_rabat').val();
+        $('.edit_h_hide-small').addClass('edit-small');
+        $('.edit-small').removeClass('edit_h_hide-small');
+        $('#product_rabat').hide();
+        $('.product_rabat_sc').hide();
+        $('.product_rabat_span').text(product_rabat_new);
+        $('#product_rabat').attr('value', product_rabat_new);
+        $('.currency_rabat').show();
+        $('.product_rabat_span').show();
+        $('.product-rabat').children(".saved-msg").animate({opacity: '0.8'});
+        $('.product-rabat').children(".saved-msg").css('height', '20px');
+        setTimeout(function(){
+            $('.product-rabat').children(".saved-msg").animate({opacity: '0'});
+            $('.product-rabat').children(".saved-msg").animate({height: '0px'});
+          }, 2000);
+    })
+
+    $(".edit_poplatok").click(function () {
+        $('.currency_poplatok').hide();
+        let product_poplatok_w = $('.product_poplatok_span').width();
+        $('#product_poplatok').css('width','40%');
+        $('#product_poplatok').show();
+        $('.product_poplatok_sc').css('display', 'flex'); 
+        $('.product_poplatok_span').hide();
+        let product_poplatok = $('.product_poplatok_span').text();
+        $('#product_poplatok').val(product_poplatok);
+        $('.edit-small').addClass('edit_h_hide-small');
+        $('.edit_h_hide-small').removeClass('edit-small');
+        
+    })
+
+    $(".p_poplatok_close").click(function () {
+        $('.edit_h_hide-small').addClass('edit-small');
+        $('.edit-small').removeClass('edit_h_hide-small');
+        $('#product_poplatok').hide();
+        $('.product_poplatok_sc').hide();
+        $('.currency_poplatok').show();
+        $('.product_poplatok_span').show();
+        $('#product_poplatok').val(product_poplatok);
+    })
+    $(".p_poplatok_save").click(function () {
+        let product_poplatok_new = $('#product_poplatok').val();
+        $('.edit_h_hide-small').addClass('edit-small');
+        $('.edit-small').removeClass('edit_h_hide-small');
+        $('#product_poplatok').hide();
+        $('.product_poplatok_sc').hide();
+        $('.product_poplatok_span').text(product_poplatok_new);
+        $('#product_poplatok').attr('value', product_poplatok_new);
+        $('.currency_poplatok').show();
+        $('.product_poplatok_span').show();
+        $('.product-poplatok').children(".saved-msg").animate({opacity: '0.8'});
+        $('.product-poplatok').children(".saved-msg").css('height', '20px');
+        setTimeout(function(){
+            $('.product-poplatok').children(".saved-msg").animate({opacity: '0'});
+            $('.product-poplatok').children(".saved-msg").animate({height: '0px'});
+          }, 2000);
+    })
+
+    $(".edit_popis").click(function () {
+        $('.currency_popis').hide();
+        let product_popis_w = $('.product_popis_span').width();
+        let product_popis_h = $('.product_popis_span').height();
+        $('#product_popis').css('width',product_popis_w);
+        $('#product_popis').css('height','80vh');
+        $('#product_popis').show();
+        $('.product_popis_sc').css('display', 'flex'); 
+        $('.product_popis_span').hide();
+        let product_popis = $('.product_popis_span').text();
+        $('#product_popis').val(product_popis);
+        $('.edit-small').addClass('edit_h_hide-small');
+        $('.edit_h_hide-small').removeClass('edit-small');
+        
+    })
+
+    $(".p_popis_close").click(function () {
+        $('.edit_h_hide-small').addClass('edit-small');
+        $('.edit-small').removeClass('edit_h_hide-small');
+        $('#product_popis').hide();
+        $('.product_popis_sc').hide();
+        $('.currency_popis').show();
+        $('.product_popis_span').show();
+        $('#product_popis').val(product_popis);
+    })
+    $(".p_popis_save").click(function () {
+        let product_popis_new = $('#product_popis').val();
+        $('.edit_h_hide-small').addClass('edit-small');
+        $('.edit-small').removeClass('edit_h_hide-small');
+        $('#product_popis').hide();
+        $('.product_popis_sc').hide();
+        $('.product_popis_span').text(product_popis_new);
+        $('#product_popis').attr('value', product_popis_new);
+        $('.currency_popis').show();
+        $('.product_popis_span').show();
+        $('.product-popis-div').children(".saved-msg").animate({opacity: '0.8'});
+        $('.product-popis-div').children(".saved-msg").css('height', '20px');
+        setTimeout(function(){
+            $('.product-popis-div').children(".saved-msg").animate({opacity: '0'});
+            $('.product-popis-div').children(".saved-msg").animate({height: '0px'});
+          }, 2000);
+    })
+
+    $(".add-akcia-btn").click(function () {
+        $('.error-akcia').hide();
+        $('.info-akcia').show();
+    })
+
+    $(".add-btn-cena").click(function () {
+        $('.akcia-decision').hide();
+        $('.akcia-percenta').hide();
+        $('.akcia-cena').show();
+        $('.akcia-edit').show();
+    })
+
+    $(".add-btn-percenta").click(function () {
+        $('.akcia-decision').hide();
+        $('.akcia-cena').hide();
+        $('.akcia-percenta').show();
+        $('.akcia-edit').show();
     })
 
     });
