1633c56d36b6a8e4e33de2d1e36195ffedcb63fc
luxxx <jahodaluk82@gmail.com>
2021-06-17 21:40:46 +0200

    pridané js files to admin

---
+++ admin/assets/js/formFix.js
@@ -0,0 +1,17 @@
+function OpravaTel(obj) {
+    var pole = obj.value.replace(/\D/g, ''),
+        char = {4:' ',7:' '};
+    obj.value = '';
+    for (var i = 0; i < pole.length; i++) {
+        obj.value += (char[i]||'') + pole[i];
+    }
+}
+
+function OpravaPsc(obj) {
+    var pole = obj.value.replace(/\D/g, ''),
+        char = {3:' '};
+    obj.value = '';
+    for (var i = 0; i < pole.length; i++) {
+        obj.value += (char[i]||'') + pole[i];
+    }
+}
