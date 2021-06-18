37081fb94d98f2719ca5ba8770e72d5cf5c5ea7f
luxxx <jahodaluk82@gmail.com>
2021-06-18 09:56:31 +0200

    obrýzky kus ine

--- includes/index-carousel.php
+++ includes/index-carousel.php
@@ -6,13 +6,13 @@ require_once "head.php";
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                <img loading="lazy" class="d-block  mx-auto" src="assets/images/carousel/ekasa1.jpg" alt="First slide">
+                <img loading="lazy" class="d-block  mx-auto" src="assets/images/carousel/ekasa1.jpg" style="width: 100%" alt="First slide">
                 </div>
                 <div class="carousel-item">
-                <img loading="lazy" class="d-block  mx-auto" src="assets/images/carousel/techsavers2.jpg" alt="Second slide">
+                <img loading="lazy" class="d-block  mx-auto" src="assets/images/carousel/techsavers2.jpg" style="width: 100%" alt="Second slide">
                 </div>
                 <div class="carousel-item">
-                <img loading="lazy" class="d-block  mx-auto" src="assets/images/carousel/techsavers1.jpg" alt="Third slide">
+                <img loading="lazy" class="d-block  mx-auto" src="assets/images/carousel/techsavers1.jpg" style="width: 100%" alt="Third slide">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
