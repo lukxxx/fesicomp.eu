1a8363ce737759d7962a0da63e32e25b6f96c42a
luxxx <jahodaluk82@gmail.com>
2021-07-10 16:07:02 +0200

    adding to cart revisited

--- add-cart.php
+++ add-cart.php
@@ -1,14 +1,14 @@
 <?php
 require_once "config.php";
 
-$quantity = $_POST["quantity"];
-$productCode = $_POST["productCode"];
+$quantity = $_GET["quantity"];
+$productCode = $_GET["p_code"];
 
 $cart = isset($_COOKIE["cart"]) ? $_COOKIE["cart"] : "[]";
 $cart = json_decode($cart);
 
 $result = mysqli_query($link, "SELECT p_id, p_nazov, p_kod_sklad, p_img, p_sklad, p_cena FROM produkty WHERE p_id = '" . $productCode . "'");
-$product = mysqli_fetch_object($result);
+$product = mysqli_fetch_object($result); 
 
 array_push($cart, array(
     "productCode" => $productCode,
